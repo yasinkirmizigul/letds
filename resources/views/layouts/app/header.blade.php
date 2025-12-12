556550fedc2d8f647e4a3ae77575b8d14346ad0e
yasn <yasinkirmizigul@gmail.com>
2025-12-12 15:42:53 +0300

    third commit

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -2,7 +2,7 @@
 <header class="kt-header fixed end-0 start-0 top-0 z-10 flex shrink-0 items-stretch bg-background" data-kt-sticky="true"
     data-kt-sticky-class="border-b border-border" data-kt-sticky-name="header" id="header">
     <!-- Container -->
-    <div class="kt-container-fixed flex items-stretch justify-between lg:gap-4" id="headerContainer">
+    <div class="flex items-stretch justify-end kt-container-fixed lg:gap-2" id="headerContainer">
         <!-- Mobile Logo -->
         <div class="-ms-1 flex items-center gap-2.5 lg:hidden">
             <a class="shrink-0" href="#">
@@ -19,8 +19,6 @@
                 </button>
             </div>
         </div>
-        <!-- End of Mobile Logo -->
-        @include('partials.mega-menu')
         <!-- Topbar -->
         <div class="flex items-center gap-2.5">
             @include('partials.topbar-search-modal')
