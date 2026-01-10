c112f5ad39421097d22ed3ee230b3be87971259b
yasn <yasinkirmizigul@gmail.com>
2026-01-10 23:12:10 +0300

    refactor + fix

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])
+Route::get('/login', [AuthController::class, 'showLogin'])
     ->name('login');
 
 Route::post('/login', [AuthController::class, 'login'])
